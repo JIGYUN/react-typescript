8f714d07b389bf548e4168f90d97defc2eacf299
정지균 <wlrbs1492@tricycle.co.kr>
2021-06-24 18:29:16 +0900

    로그인추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import TodoPage from "./todo/page/TodoPage";
 import ChatPage from "./chat/page/ChatPage";
+import LoginPage from "./login/page/LoginPage";
+
+import PrivateRoute from "./PrivateRoute";
+
 
 class App extends Component{
 
@@ -12,11 +16,12 @@ class App extends Component{
     return(
       <div>
         <Router>
-            <Redirect to="/chat" />
-            <Route path="/chat" component={ChatPage} />  
-            <Route path="/todo" component={TodoPage} /> 
+            <Redirect to="/login" />
+            <Route path="/login" component={LoginPage} />
+            <PrivateRoute path="/chat" component={ChatPage} />
+            <PrivateRoute path="/todo" component={TodoPage} />
         </Router>
-      </div>
+      </div>  
     ) 
   }
 
